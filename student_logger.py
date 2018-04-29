7d3a4bdd04d207541be85df0c6de693a64509d55
gulab3483 <37915032+gulab3483@users.noreply.github.com>
2018-04-29 13:41:16 -0700

    hunkjbg
    move buttons

--- student_logger.py
+++ student_logger.py
@@ -19,27 +19,22 @@ def printStudents():
 	for i in my_dict.keys():
 		text = w.cget("text") + "Student Name: {}\tStudent ID: {}\tStudent Courses: {}\n".format(my_dict[i]['name'], i, my_dict[i]['Courses'])
 		w.configure(text=text)
-	w.grid(column=3)
+	w.grid(column=3, row=0)
 
 #OutputFrame():
-OutputFrame = tk.Frame(root, width = 500, height = 300, bg="green")
+OutputFrame = tk.Frame(root, width = 500, height = 300)
 OutputFrame.grid(column=3)
 
 #Text pane:
-textPane = tk.Frame(root, width=500, height = 150, bg="red")
+textPane = tk.Frame(root, width=500, height = 150)
 textPane.grid(column=3)
 
 #Button pane:
-buttonFrame = tk.Frame(root, width=200, height=200, bg="blue")
+buttonFrame = tk.Frame(root, width=200, height=200)
 buttonFrame.grid(row=0, column=0)
 
-
-#Buttons:
-#psButton = tk.Button(buttonFrame, text="Print Students", command=printStudents)
-# psButton.grid(row=0, column=0)
-# asButton = tk.Button(buttonFrame, text="Add Student", command=addStudent)
-# asButton.grid(row=0, column=0)
-
+psButton = tk.Button(buttonFrame, text="Print Students", command=printStudents)
+psButton.grid(row=0, column=0)
 
 # my_dict['4723']['Courses'].append('Economics')
 # addStudent('9537', 'Tom',['math', 'science', 'doingstuff'])
